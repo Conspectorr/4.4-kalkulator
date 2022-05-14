f6fd9f1b50aaa612d13cf5e94d8bb9af10566588
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-14 19:39:56 +0200

    Change words

--- 4.4-kalkulator.py
+++ 4.4-kalkulator.py
@@ -8,16 +8,16 @@ def kalkulator(działanie):
     liczba1 = int(input("Podaj składnik 1: "))
     liczba2 = int(input("Podaj składnik 2: "))
     if działanie == "1":
-        logging.info("Wykonasz dodawanie %s i %s" % (liczba1, liczba2))
+        logging.info("Wykonujesz dodawanie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1+liczba2)
     elif działanie == "2":
-        logging.info("Wykonasz odejmowanie %s i %s" % (liczba1, liczba2))
+        logging.info("Wykonujesz odejmowanie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1-liczba2)
     elif działanie == "3":
-        logging.info("Wykonasz mnożenie %s i %s" % (liczba1, liczba2))
+        logging.info("Wykonujesz mnożenie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1*liczba2)
     elif działanie == "4":
-        logging.info("Wykonasz dzielenie %s i %s" % (liczba1, liczba2))
+        logging.info("Wykonujesz dzielenie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1/liczba2)
 
 kalkulator(działanie)
