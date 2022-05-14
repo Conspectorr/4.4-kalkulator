c6b3d66acfedb015d9a58699cae4ab6e95e9817e
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-14 19:35:34 +0200

    Add options

--- 4.4-kalkulator.py
+++ 4.4-kalkulator.py
@@ -5,15 +5,19 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 print("Witaj w tym prostym kalkulatorze, co chciałbyś policzyć?")
 działanie = input("Podaj działanie, posługując się odpowiednią liczbą:  \n1- dodawanie,  \n2- odejmowanie,  \n3- mnożenie,  \n4- dzielenie:  \n")
 def kalkulator(działanie):
-    liczba1 = input("Podaj składnik 1: ")
-    liczba2 = input("Podaj składnik 2: ")
+    liczba1 = int(input("Podaj składnik 1: "))
+    liczba2 = int(input("Podaj składnik 2: "))
     if działanie == "1":
         logging.info("Wykonasz dodawanie %s i %s" % (liczba1, liczba2))
+        print("Wynik to:", liczba1+liczba2)
     elif działanie == "2":
         logging.info("Wykonasz odejmowanie %s i %s" % (liczba1, liczba2))
+        print("Wynik to:", liczba1-liczba2)
     elif działanie == "3":
-        logging.info("Wykonasz dzielenie %s i %s" % (liczba1, liczba2))
-    elif działanie == "4":
         logging.info("Wykonasz mnożenie %s i %s" % (liczba1, liczba2))
+        print("Wynik to:", liczba1*liczba2)
+    elif działanie == "4":
+        logging.info("Wykonasz dzielenie %s i %s" % (liczba1, liczba2))
+        print("Wynik to:", liczba1/liczba2)
 
 kalkulator(działanie)
