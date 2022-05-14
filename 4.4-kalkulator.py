406310efb6a618da67fd057069a3091b7043d34b
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-14 20:23:15 +0200

    Add new value system

--- 4.4-kalkulator.py
+++ 4.4-kalkulator.py
@@ -1,23 +1,37 @@
+from ast import Mult
 import sys
 import logging
+import operator
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 print("Witaj w tym prostym kalkulatorze, co chciałbyś policzyć?")
-działanie = input("Podaj działanie, posługując się odpowiednią liczbą:  \n1- dodawanie,  \n2- odejmowanie,  \n3- mnożenie,  \n4- dzielenie:  \n")
+działanie = input("Podaj działanie, posługując się odpowiednią liczbą:  \nDodawanie: 1  \nOdejmowanie: 2  \nMnożenie: 3  \nDzielenie: 4\n")
 def kalkulator(działanie):
-    liczba1 = int(input("Podaj składnik 1: "))
-    liczba2 = int(input("Podaj składnik 2: "))
     if działanie == "1":
-        logging.info("Wykonujesz dodawanie %s i %s" % (liczba1, liczba2))
-        print("Wynik to:", liczba1+liczba2)
+      liczby_text=input("Podaj dodawane wartości, rozdzielając je przecinkiem:")
+      liczby = liczby_text.split(',')
+      liczby2=[int(i) for i in liczby]
+      logging.info("Wykonujesz dodawanie %s" % liczby2)
+      print("Wynik to:", sum(liczby2))
     elif działanie == "2":
+        liczba1 = int(input("Podaj wartość 1: "))
+        liczba2 = int(input("Podaj wartość 2: "))
         logging.info("Wykonujesz odejmowanie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1-liczba2)
     elif działanie == "3":
-        logging.info("Wykonujesz mnożenie %s i %s" % (liczba1, liczba2))
-        print("Wynik to:", liczba1*liczba2)
+        liczby_text=input("Podaj mnożone wartości, rozdzielając je przecinkiem:")
+        liczby = liczby_text.split(',')
+        liczby2=[int(i) for i in liczby]
+        logging.info("Wykonujesz mnożenie %s" % liczby2)
+        print("Wynik to:", sum(liczby2))
+#Brak mi wbudowanej funkcji dla takiego działania chyba, więc trzeba własną...
     elif działanie == "4":
+        liczba1 = int(input("Podaj wartość 1: "))
+        liczba2 = int(input("Podaj wartość 2: "))
         logging.info("Wykonujesz dzielenie %s i %s" % (liczba1, liczba2))
         print("Wynik to:", liczba1/liczba2)
+    else:
+        print("Wybierz prawidłowe działanie!")
 
 kalkulator(działanie)
+print("Do zobaczenia w kolejnych działaniach!")
